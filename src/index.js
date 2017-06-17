e7f923913e63522c287d1d918664c88851d3e0ee
Dmitry Ryumkin <riumkin@gmail.com>
2017-06-17 19:42:21 +0300

    Add youtube search response

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyD7AHS7u7h5aYC380mapDvt5NXBEi4IZ10';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
 const App = () => {
   return (
