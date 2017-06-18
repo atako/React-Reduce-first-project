efd8d41b4bbcd05a9965bd812a2e7721410a0a18
Dmitry Ryumkin <riumkin@gmail.com>
2017-06-18 10:35:43 +0300

    add delay to search

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -31,9 +32,12 @@ class App extends Component {
   }
 
   render() { 
+
+    const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 700);
+
     return (
       <div>
-        <SearchBar onSearchTermSearch={term => this.videoSearch(term)} />
+        <SearchBar onSearchTermSearch={videoSearch} />
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList 
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
